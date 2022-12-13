6a05ea233023b2ad8c583d6178e864318fb8c6cf
MrMikey695 <mikeedubas@hotmail.com>
2022-12-13 07:11:40 +0200

    update

--- sources/exec_tree/exec_cmd.c
+++ sources/exec_tree/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 18:12:53 by jakken            #+#    #+#             */
-/*   Updated: 2022/12/12 20:38:57 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/12/13 07:10:51 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,6 @@ void	execute_bin(char **args, char ***environ_cp, t_session *sesh)
 
 	if (!args[0])
 		return ;
-	ft_printf("bin %d\n", sesh->head->type);
 	if (!ft_builtins(sesh, &args))
 		return ;
 	if (!check_if_user_exe(args[0], &cmd))
