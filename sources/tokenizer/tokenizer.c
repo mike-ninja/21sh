fe9259b0acf686b6ad6b657305b5dcb6590414a8
jniemine <jniemine@c1r3p5.hive.fi>
2022-12-13 22:03:27 +0200

    Started norming tokenizer, need to test tomorrow

--- sources/tokenizer/tokenizer.c
+++ sources/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: jakken <jakken@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:15:33 by jniemine          #+#    #+#             */
-/*   Updated: 2022/12/13 21:17:51 by jakken           ###   ########.fr       */
+/*   Updated: 2022/12/13 22:01:40 by jakken           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void free_tokens(t_token *tokens)
 	}
 	ft_memdel((void **)&tokens);
 }
-
 
 void	track_used_space(t_token **args, size_t current_pointer_n
 	, size_t *max_pointer_n)
@@ -52,55 +51,12 @@ void	track_used_space(t_token **args, size_t current_pointer_n
 	}
 }
 
-/* TODO add rest */
 int	is_seperator(char c)
 {
 	return (is_nl(c) || c == '|' || c == '>' || c == '<'
 			|| c == ';');
 
 }
-
-
-
-char	*if_redir(char *line, int *i, int *start, int *end)
-{
-	int digits;
-
-	digits = 0;
-	while (ft_isdigit(line[*i + digits]))
-		++digits;
-	if (line[*i + digits] == '>' || line[*i + digits] == '<')
-	{
-		*start = *i;
-		*i = *i + digits;
-		*end = *i + digits + 1;
-		if (line[*end] == '<' || line[*end] == '>' || line[*end] == '&')
-			++(*end);
-		if (*end && line[(*end) - 1] == '&' && line[*end] == '-')
-			return(ft_strsub(line, *start, (++(*end)) - *start));
-		if (line[*end] == '<' || line[*end] == '>' || line[*end] == '&')
-		{
-			ft_printf("21sh: syntax error near `%c'", line[*end]);
-			//Freeall
-			return (NULL);
-		}
-		//What should be done with arguments touching -?
-	//	if (line[*end] == '-')
-	//	{
-	//		while (line[*end] && !is_ws(line[*end]) && !is_seperator(line[*end]))
-	//			++(*end);
-	//		return(ft_strsub(line, *start, *end - *start));
-	//	}
-		while (line[*end] && is_ws(line[*end]))
-			++(*end);
-		while (line[*end] && !is_ws(line[*end]) && !is_seperator(line[*end]))
-			++(*end);
-		return(ft_strsub(line, *start, *end - *start));
-	}
-	return (NULL);
-}
-
-
 
 void	set_token_values(t_token *token, int token_id, char *value)
 {
@@ -109,22 +65,26 @@ void	set_token_values(t_token *token, int token_id, char *value)
 		token->value = value;
 }
 
-
-/*
-static void redir_or_aggregation(char *line, int prev, int cur)
+static void	init_token(char *c, t_token *token, char *line, int cur)
 {
-	while (prev < cur && )
+	if (*c == '|')
+		set_token_values(token, PIPE, c);
+	else if (line[cur] == '>' || line[cur] == '<')
+	{
+		if (line[cur + 1] && line[cur + 2] && line[cur + 1] == '&'
+			&& line[cur + 2] == '-')
+			set_token_values(token, CLOSEFD, c);
+		else if (line[cur + 1] == '&')
+			set_token_values(token, AGGREGATION, c);
+		else
+			set_token_values(token, REDIR, c);
+	}
+	else if (*c == ';')
+		set_token_values(token, SEMICOLON, c);
+	else
+		set_token_values(token, WORD, c);
 }
-*/
 
-/* TODO add support for && and || while ur at it */
-/*	TODO 2randomfile>something because source is not only numbers it is assumed to be filename*/
-/* Add ( and ) */
-/* Make unit tests */
-//TODO make token node for fd aggregation
-//TODO >& must have digit target
-//TODO combine all word tokens before ; as one...
-// TODO Pick up only touching digits before
 t_token	*chop_line(char *line, t_token *args, size_t pointer_n)
 {
 	int		i_args;
@@ -147,22 +107,23 @@ t_token	*chop_line(char *line, t_token *args, size_t pointer_n)
 	while (line[cur])
 	{
 		c = find_argument(line, &cur, &start, &end);
-		if (*c == '|')
-			set_token_values(&args[i_args], PIPE, c);
-		else if (line[cur] == '>' || line[cur] == '<')
-		{
-			if (line[cur + 1] && line[cur + 2] && line[cur + 1] == '&'
-				&& line[cur + 2] == '-')
-				set_token_values(&args[i_args], CLOSEFD, c);
-			else if (line[cur + 1] == '&')
-				set_token_values(&args[i_args], AGGREGATION, c);
-			else
-				set_token_values(&args[i_args], REDIR, c);
-		}
-		else if (*c == ';')
-			set_token_values(&args[i_args], SEMICOLON, c);
-		else
-			set_token_values(&args[i_args], WORD, c);
+		init_token(c, &args[i_args], line, cur);
+//		if (*c == '|')
+//			set_token_values(&args[i_args], PIPE, c);
+//		else if (line[cur] == '>' || line[cur] == '<')
+//		{
+//			if (line[cur + 1] && line[cur + 2] && line[cur + 1] == '&'
+//				&& line[cur + 2] == '-')
+//				set_token_values(&args[i_args], CLOSEFD, c);
+//			else if (line[cur + 1] == '&')
+//				set_token_values(&args[i_args], AGGREGATION, c);
+//			else
+//				set_token_values(&args[i_args], REDIR, c);
+//		}
+//		else if (*c == ';')
+//			set_token_values(&args[i_args], SEMICOLON, c);
+//		else
+//			set_token_values(&args[i_args], WORD, c);
 		++i_args;
 		track_used_space(&args, i_args, &pointer_n);
 		while (is_ws(line[end]))
