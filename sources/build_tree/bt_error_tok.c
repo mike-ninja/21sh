5433e474a11f928cab477cb29c9f1e46e0e16baa
jniemine <jniemine@student.hive.fi>
2022-12-14 21:25:26 +0200

    Normed build_tree

--- sources/build_tree/bt_error_tok.c
+++ sources/build_tree/bt_error_tok.c
@@ -6,13 +6,13 @@
 /*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:32:10 by jniemine          #+#    #+#             */
-/*   Updated: 2022/12/14 21:15:49 by jniemine         ###   ########.fr       */
+/*   Updated: 2022/12/14 21:20:58 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
 
-static void free_cmd_node(t_cmdnode *node)
+static void	free_cmd_node(t_cmdnode *node)
 {
 	if (node)
 	{
@@ -24,14 +24,14 @@ static void free_cmd_node(t_cmdnode *node)
 	}
 }
 
-static void free_redir_node(t_redir **redir)
+static void	free_redir_node(t_redir **redir)
 {
 	free_cmd_node((t_cmdnode *)(*redir)->cmd);
 	ft_memdel((void **)&(*redir)->filepath);
 	*redir = NULL;
 }
 
-int error_tok(t_token *tokens, t_treenode *redir_head, char *msg, char *symbol)
+int	error_tok(t_token *tokens, t_treenode *redir_head, char *msg, char *symbol)
 {
 	if (!symbol)
 		ft_err_print(NULL, NULL, msg, 2);
