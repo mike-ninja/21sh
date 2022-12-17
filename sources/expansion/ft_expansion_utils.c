ebc0a5ccf55469781031e4263710a4abb4af1537
MrMikey695 <mikeedubas@hotmail.com>
2022-12-17 16:15:18 +0200

    improved algorithim for removing nl, quotes, and backslashed right after expansion

--- sources/expansion/ft_expansion_utils.c
+++ sources/expansion/ft_expansion_utils.c
@@ -6,73 +6,73 @@
 /*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 13:38:31 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/12/17 13:45:53 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/12/17 16:14:40 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
 
-static int	ft_qoute_handling(char *buff, int k, char *quote)
+static int	ft_bslash_null(char *buff, ssize_t pos)
 {
-	int	i;
+	ssize_t	count;
 
-	i = k;
-	if (!*quote)
-		*quote = buff[k];
-	else if (*quote == buff[k])
-	{
-		i++;
-		if (buff[i] == '\'' || buff[i] == '\"')
-			*quote = buff[i];
-		else
-			*quote = 0;
-	}
-	while (buff[i] && (buff[i] == *quote))
-		i++;
-	return (i);
+	if (!pos)
+		return (0);
+	count = pos - 1;
+	while (count && buff[count] == '\\')
+		count--;
+	if (!count)
+		count++;
+	if (((pos - 1) - count) % 2)
+		return (1);
+	return (0);
 }
 
-static int	blash_handling(char *buff, int k)
+static void	ft_quote_newline_parse(char *buff)
 {
-	int	i;
-
-	i = k + 1;
-	if (buff[i] == '\n')
-		i++;
-	return (i);
-}
-
-static int	newline_handling(char *buff, int *k)
-{
-	int	i;
-
-	i = *k;
-	if (*k && buff[*k - 1] == '\\')
-	{
-		(*k)--;
-		i++;
-	}
-	return (i);
-}
-
-void	ft_quote_blash_removal(char *buff)
-{
-	int		k;
 	int		i;
+	int		k;
 	char	quote;
 
 	k = -1;
 	quote = 0;
 	while (buff[++k])
 	{
-		if (buff[k] == '\\')
-			i = blash_handling(buff, k);
-		else if (buff[k] == '\n')
-			i = newline_handling(buff, &k);
-		else if ((buff[k] == '\'' || buff[k] == '\"'))
-			i = ft_qoute_handling(buff, k, &quote);
+		if (buff[k] == '\n' && ft_bslash_null(buff, k))
+			i = k + 1;
+		else if ((buff[k] == S_QUO || buff[k] == D_QUO) \
+		&& !ft_bslash_null(buff, k))
+		{
+			if (!quote)
+				quote = buff[k];
+			else if (quote == buff[k])
+				quote = 0;
+			i = k + 1;
+		}
 		else
 			i = k;
-		ft_memmove((void *)&buff[k], (void *)&buff[i], ft_strlen(&buff[i]) + 1);
+		if (i != k)
+			ft_memmove((void *)&buff[k], (void *)&buff[i], \
+			ft_strlen(&buff[i]) + 1);
 	}
+}
+
+static void	ft_bslash_parse(char *buff)
+{
+	int		len;
+
+	len = ft_strlen(buff);
+	while (len >= 0)
+	{
+		if (buff[len] == '\\' && !ft_bslash_null(buff, len))
+			ft_memmove((void *)&buff[len], (void *)&buff[len + 1], \
+			ft_strlen(&buff[len + 1]) + 1);
+		len--;
+	}
+}
+
+void	ft_quote_blash_removal(char *buff)
+{
+	ft_quote_newline_parse(buff);
+	ft_bslash_parse(buff);
 }
