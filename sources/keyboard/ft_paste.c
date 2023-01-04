461998e303c81ca1daf1d022c93dc89cdae109fa
Michel philippe Barutel <mbarutel@c1r3p4.hive.fi>
2023-01-04 12:00:35 +0200

    fixed a bug with space where the buffsize limit did not correspond with the limit inside input_cycle

--- sources/keyboard/ft_paste.c
+++ sources/keyboard/ft_paste.c
@@ -3,14 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_paste.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 13:04:10 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/12/20 15:20:56 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/01/04 11:59:32 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "keyboard.h"
+
+static void	paste_into_array(t_term *t)
+{
+	int	i;
+
+	i = -1;
+	while (t->bytes < (BUFF_SIZE - 1) && t->clipboard.buff[++i])
+	{
+		t->c_col++;
+		ft_putc(t->clipboard.buff[i]);
+		ft_shift_nl_addr(t, 1);
+		if (t->inp[t->index])
+			ft_shift_insert(t);
+		t->inp[t->index++] = t->clipboard.buff[i];
+		t->bytes++;
+		ft_trigger_nl(t);
+		if (t->c_col == t->ws_col)
+			t->c_row++;
+	}
+}
 
 /*
  * It pastes the contents of the clipboard into the input buffer
@@ -19,29 +39,15 @@
  */
 void	ft_paste(t_term *t)
 {
-	int		i;
-
-	i = -1;
 	if (t->clipboard.buff)
 	{
 		ft_run_capability("vi");
-		while (t->bytes < BUFF_SIZE && t->clipboard.buff[++i])
-		{
-			t->c_col++;
-			ft_putc(t->clipboard.buff[i]);
-			ft_shift_nl_addr(t, 1);
-			if (t->inp[t->index])
-				ft_shift_insert(t);
-			t->inp[t->index++] = t->clipboard.buff[i];
-			t->bytes++;
-			ft_trigger_nl(t);
-			if (t->c_col == t->ws_col)
-				t->c_row++;
-		}
+		paste_into_array(t);
 		if (t->inp[t->index])
 			ft_print_trail(t);
 		if (t->clipboard.type == CUT)
 			ft_strdel(&t->clipboard.buff);
+		ft_quote_flag_reset(t);
 		ft_run_capability("ve");
 	}
 }
