97645ef961688f9b303877180da960ca4b01660b
Michel philippe Barutel <mbarutel@c1r3p4.hive.fi>
2023-01-11 12:40:02 +0200

    update strtrim and lexer:

--- sources/lexer/ft_lexer.c
+++ sources/lexer/ft_lexer.c
@@ -6,7 +6,7 @@
 /*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 10:05:26 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/01/04 17:33:25 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/01/11 12:37:50 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ char	*ft_lexer(t_term *t)
 	char	*new;
 
 	new = ft_strtrim(t->inp);
-	if (!*new)
-		ft_strdel(&new);
-	new = ft_heredoc(t, new);
+	if (new)
+		new = ft_heredoc(t, new);
 	return (new);
 }
