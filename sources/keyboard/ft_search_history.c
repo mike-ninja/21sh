b78014678d27eb6e66c3da7fd5684431433d89ca
Michel philippe Barutel <mbarutel@c2r3p9.hive.fi>
2023-01-20 16:29:33 +0200

    update

--- sources/keyboard/ft_search_history.c
+++ sources/keyboard/ft_search_history.c
@@ -6,7 +6,7 @@
 /*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:36:20 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/01/20 14:56:36 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/01/20 16:28:45 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,18 @@ static int	init_interface(t_term *t, int history_rows, ssize_t start_cur_row)
 	return (start_cur_row + (history_rows - 1));
 }
 
+static void	init_index_ptr(int **ptr, int history_rows)
+{
+	int i;
+	
+	i = -1;
+	if (*ptr)
+		ft_memdel((void **)&(*ptr));
+	*ptr = (int *)ft_memalloc(sizeof(int) * (history_rows));
+	while (++i < history_rows)
+		(*ptr)[i] = -1;
+}
+
 void	ft_search_history(t_term *t)
 {
 	int			history_index;
@@ -79,23 +91,22 @@ void	ft_search_history(t_term *t)
 	history_index = t->history_size - 1;
 	start_cur_row = t->term_val[1] + t->c_row;
 	row = init_interface(t, history_rows, start_cur_row);
-
+	
 	/* Trial here */
 	ft_setcursor(0, start_cur_row + (history_rows - 1));
-	/* Init int ptr */
-	ptr = (int *)ft_memalloc(sizeof(int) * (history_rows));
-	
-	int i;
-
-	i = -1;
-	while (++i < history_rows)
-		ptr[i] = 0;
-	/* Init int ptr */
+	init_index_ptr(&ptr, history_rows);
 	index_limit = history_options(t, history_rows, start_cur_row + (history_rows - 1), history_index, ptr);
 	ft_setcursor(0, start_cur_row + (history_rows - 1));
 	print_prompt("RED");
 	/* Trial here */
 	index = index_limit;
+	ft_run_capability("sc");
+	ft_setcursor(0, t->ws_row - 1);
+	ft_run_capability("ce");
+	ft_printf("index -> %d", index);
+	ft_run_capability("rc");
+	if (index == -1)
+		return ;
 	while (true) // while loop for the selector
 	{
 		inp = ft_get_input();
@@ -112,15 +123,16 @@ void	ft_search_history(t_term *t)
 					--row;
 					--index;
 				}
-				else if ((size_t)history_index - history_rows && index_limit == (history_rows - 1))
+				// else if ((size_t)history_index - history_rows && index_limit == (history_rows - 1))
+				else if (index_limit == (history_rows - 1) && ptr[index_limit - index] > 1)
 				{
+					ft_run_capability("sc");
+					ft_setcursor(0, t->ws_row - 1);
+					ft_run_capability("ce");
+					ft_printf("ptr -> %d", index_limit);
+					ft_run_capability("rc");
 					--history_index;
-					ft_memdel((void **)&ptr);
-					ptr = (int *)ft_memalloc(sizeof(int) * (history_rows));
-
-					i = -1;
-					while (++i < history_rows)
-						ptr[i] = 0;
+					init_index_ptr(&ptr, history_rows);
 					index_limit = history_options(t, 10, start_cur_row + (history_rows - 1), history_index, ptr);
 				}
 				ft_setcursor(0, row);
@@ -138,12 +150,7 @@ void	ft_search_history(t_term *t)
 				else if ((size_t)history_index < (t->history_size - 1))
 				{
 					++history_index;
-					ft_memdel((void **)&ptr);
-					ptr = (int *)ft_memalloc(sizeof(int) * (history_rows));
-
-					i = -1;
-					while (++i < history_rows)
-						ptr[i] = 0;
+					init_index_ptr(&ptr, history_rows);
 					index_limit = history_options(t, 10, start_cur_row + (history_rows - 1), history_index, ptr);	
 				}
 				ft_setcursor(0, row);
@@ -160,3 +167,103 @@ void	ft_search_history(t_term *t)
 	}
 	ft_run_capability("ve");
 }
+
+// void	ft_search_history(t_term *t)
+// {
+// 	int			history_index;
+// 	int			history_rows;
+// 	ssize_t		start_cur_row;
+// 	ssize_t		index_limit;
+// 	ssize_t		index;
+// 	ssize_t		row;
+// 	char		inp;
+// 	int			*ptr;	
+	
+
+// 	ptr = NULL;
+// 	ft_run_capability("vi");
+// 	history_rows = 10;
+// 	history_index = t->history_size - 1;
+// 	start_cur_row = t->term_val[1] + t->c_row;
+// 	row = init_interface(t, history_rows, start_cur_row);
+
+// 	/* Trial here */
+// 	ft_setcursor(0, start_cur_row + (history_rows - 1));
+// 	/* Init int ptr */
+// 	ptr = (int *)ft_memalloc(sizeof(int) * (history_rows));
+	
+// 	int i;
+
+// 	i = -1;
+// 	while (++i < history_rows)
+// 		ptr[i] = 0;
+// 	/* Init int ptr */
+// 	index_limit = history_options(t, history_rows, start_cur_row + (history_rows - 1), history_index, ptr);
+// 	ft_setcursor(0, start_cur_row + (history_rows - 1));
+// 	print_prompt("RED");
+// 	/* Trial here */
+// 	index = index_limit;
+// 	while (true) // while loop for the selector
+// 	{
+// 		inp = ft_get_input();
+// 		if (inp == 'q')
+// 			break;
+// 		if (inp == 91)
+// 		{
+// 			inp = ft_get_input();
+// 			if (inp == 65) // up
+// 			{
+// 				if (index)
+// 				{
+// 					ft_run_capability("cb");
+// 					--row;
+// 					--index;
+// 				}
+// 				else if ((size_t)history_index - history_rows && index_limit == (history_rows - 1))
+// 				{
+// 					--history_index;
+// 					ft_memdel((void **)&ptr);
+// 					ptr = (int *)ft_memalloc(sizeof(int) * (history_rows));
+
+// 					i = -1;
+// 					while (++i < history_rows)
+// 						ptr[i] = 0;
+// 					index_limit = history_options(t, 10, start_cur_row + (history_rows - 1), history_index, ptr);
+// 				}
+// 				ft_setcursor(0, row);
+// 				print_prompt("RED");
+					
+// 			}
+// 			if (inp == 66) // do
+// 			{
+// 				if (index < index_limit)
+// 				{
+// 					ft_run_capability("cb");
+// 					++row;
+// 					++index;
+// 				}
+// 				else if ((size_t)history_index < (t->history_size - 1))
+// 				{
+// 					++history_index;
+// 					ft_memdel((void **)&ptr);
+// 					ptr = (int *)ft_memalloc(sizeof(int) * (history_rows));
+
+// 					i = -1;
+// 					while (++i < history_rows)
+// 						ptr[i] = 0;
+// 					index_limit = history_options(t, 10, start_cur_row + (history_rows - 1), history_index, ptr);	
+// 				}
+// 				ft_setcursor(0, row);
+// 				print_prompt("RED");
+// 			}
+// 		}
+// 		if (inp == '\n')
+// 		{
+// 			ft_run_capability("cl");
+// 			ft_printf("|%s|", t->history_arr[ptr[index_limit - index]]);
+// 			ft_memdel((void **)&ptr);
+// 			break;
+// 		}
+// 	}
+// 	ft_run_capability("ve");
+// }
