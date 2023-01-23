9f5ace0fec044b0a63788702234736b27942fc54
Michel philippe Barutel <mbarutel@c2r3p9.hive.fi>
2023-01-23 10:57:22 +0200

    cleaned up

--- sources/keyboard/ft_search_history.c
+++ sources/keyboard/ft_search_history.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_search_history.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:36:20 by mbarutel          #+#    #+#             */
-/*   Updated: 2023/01/22 22:12:57 by mbarutel         ###   ########.fr       */
+/*   Updated: 2023/01/23 10:57:01 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,6 @@ void	init_index_ptr(t_search_history *config)
 	int i;
 	
 	i = -1;
-	// if (config->ptr)
-	// 	ft_memdel((void **)&config->ptr);
 	config->ptr = (int *)ft_memalloc(sizeof(int) * (config->history_rows));
 	while (++i < config->history_rows)
 		config->ptr[i] = -1;
@@ -100,7 +98,7 @@ void	init_interface(t_term *t, t_search_history *config)
 	ft_display_to_show(config);
 }
 
-void	history_options(t_term *t, t_search_history *config) // This should return the amount of lines printed
+void	history_options(t_term *t, t_search_history *config)
 {
 	int	index;
 	int	row_cpy;
@@ -113,7 +111,6 @@ void	history_options(t_term *t, t_search_history *config) // This should return 
 	row_cpy = config->history_rows;
 	history_index_cpy = config->history_index;
 	to_show_cpy = config->to_show + config->history_rows;
-	// display_row_cpy =  config->start_cur_row + (config->history_rows - 1);
 	while (row_cpy && history_index_cpy && t->history_arr[history_index_cpy] && to_show_cpy && index < config->match)
 	{
 		if (ft_strstr(t->history_arr[history_index_cpy], t->nl_addr[t->total_row])) // This logic needs to be improved
@@ -130,6 +127,68 @@ void	history_options(t_term *t, t_search_history *config) // This should return 
 		history_index_cpy--;
 	}
 	config->index = (config->history_rows - row_cpy) - 1;
+}
+
+static void	ft_selector_do(int *index_cpy, int *row_cpy, t_term *t, t_search_history *config)
+{
+	if ((*index_cpy) < config->index)
+	{ 
+		++(*row_cpy);
+		++(*index_cpy);
+		++config->to_show;
+		ft_run_capability("cb");
+	}
+	else if ((size_t)config->history_index < (t->history_size - 1))
+	{
+		++config->to_show;
+		++config->history_index;
+		history_options(t, config);
+	}
+	ft_setcursor(0, *row_cpy);
+	print_prompt("RED");
+}
+
+static void	ft_selector_up(int *index_cpy, int *row_cpy, t_term *t, t_search_history *config)
+{
+	if (*index_cpy)
+	{
+		--(*row_cpy);
+		--(*index_cpy);
+		--config->to_show;
+		ft_run_capability("cb");
+	}
+	else if (config->index == (config->history_rows - 1) && config->ptr[config->index - (*index_cpy)] > 1)
+	{
+		--config->to_show;
+		--config->history_index;
+		history_options(t, config);
+	}
+	ft_setcursor(0, *row_cpy);
+	print_prompt("RED");
+}
+
+static void	ft_select_history(t_term *t, t_search_history *config, int index_cpy)
+{
+	t->c_col = ft_strlen(t->history_arr[config->ptr[config->index - index_cpy]]);
+	t->bytes -= ft_strlen(t->nl_addr[t->total_row]);
+	ft_memcpy(t->nl_addr[t->total_row], t->history_arr[config->ptr[config->index - index_cpy]], t->c_col);
+	t->term_val[1] = config->start_cur_row;
+	t->bytes += t->c_col;
+	t->index = t->bytes;
+	ft_setcursor(0, config->start_cur_row);
+	ft_run_capability("cd");
+	if (!t->total_row)
+	{
+		ft_printf("{GREEN}%s{RESET}", PROMPT);
+		t->c_col += t->prompt_len;
+	}
+	else
+	{
+		ft_printf("{GREEN}%s{RESET}", MINI_PROMPT);
+		t->c_col += t->m_prompt_len;
+	}
+	ft_putstr(t->nl_addr[t->total_row]);
+	ft_memdel((void **)&config->ptr);	
 }
 
 void	ft_search_history(t_term *t)
@@ -160,66 +219,13 @@ void	ft_search_history(t_term *t)
 		{
 			config.inp = ft_get_input();
 			if (config.inp == 65 && config.to_show) // up
-			{
-				if (index_cpy)
-				{
-					--row_cpy;
-					--index_cpy;
-					--config.to_show;
-					ft_run_capability("cb");
-				}
-				else if (config.index == (config.history_rows - 1) && config.ptr[config.index - index_cpy] > 1)
-				{
-					--config.to_show;
-					--config.history_index;
-					history_options(t, &config);
-				}
-				ft_setcursor(0, row_cpy);
-				print_prompt("RED");
-					
-			}
+				ft_selector_up(&index_cpy, &row_cpy, t, &config);
 			if (config.inp == 66 && config.to_show < config.max_to_show) // do
-			{
-				if (index_cpy < config.index)
-				{
-					++row_cpy;
-					++index_cpy;
-					++config.to_show;
-					ft_run_capability("cb");
-				}
-				else if ((size_t)config.history_index < (t->history_size - 1))
-				{
-					++config.to_show;
-					++config.history_index;
-					history_options(t, &config);
-				}
-				ft_setcursor(0, row_cpy);
-				print_prompt("RED");
-			}
+				ft_selector_do(&index_cpy, &row_cpy, t, &config);
 		}
 		if (config.inp == '\n')
 		{
-			t->c_col = ft_strlen(t->history_arr[config.ptr[config.index - index_cpy]]);
-			t->bytes -= ft_strlen(t->nl_addr[t->total_row]);
-			ft_memcpy(t->nl_addr[t->total_row], t->history_arr[config.ptr[config.index - index_cpy]], t->c_col);
-			t->term_val[1] = config.start_cur_row;
-			t->bytes += t->c_col;
-			t->index = t->bytes;
-			ft_setcursor(0, config.start_cur_row);
-			ft_run_capability("cd");
-			if (!t->total_row)
-			{
-				ft_printf("{GREEN}%s{RESET}", PROMPT);
-				t->c_col += t->prompt_len;
-			}
-			else
-			{
-				ft_printf("{GREEN}%s{RESET}", MINI_PROMPT);
-				t->c_col += t->m_prompt_len;
-			}
-			ft_putstr(t->nl_addr[t->total_row]);
-			
-			ft_memdel((void **)&config.ptr);
+			ft_select_history(t, &config, index_cpy);
 			break;
 		}
 	}
